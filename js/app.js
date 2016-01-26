d4105b612a3dcf8905223ce8a3235f40275e4f31
Ian Adams <ianadams@Ian-Adams.local>
2016-01-26 16:46:21 -0600

    end of day 2 coding

--- js/app.js
+++ js/app.js
@@ -22,3 +22,40 @@ var app = (function(document, $) {
 (function() {
 	app.init();
 })();
+
+
+
+function initMap() {
+
+  // Specify features and elements to define styles.
+  var styleArray = [
+    {
+      featureType: "all",
+      stylers: [
+       { saturation: -80 }
+      ]
+    },{
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#00ffee" },
+        { saturation: 50 }
+      ]
+    },{
+      featureType: "poi.business",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    // Apply the map style array to the map.
+    styles: styleArray,
+    zoom: 8
+  });
+}
